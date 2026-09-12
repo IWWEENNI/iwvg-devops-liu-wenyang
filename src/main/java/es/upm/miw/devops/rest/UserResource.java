f5525006c78a59954a5f3b64b7452d5f9c86ea79
IWWEENNI <123wenyang@gmail.com>
2026-09-12 13:26:24 +0200

    Añadir el endpoint: PUT /user/{id}/active, sin tests. refs #7.

--- src/main/java/es/upm/miw/devops/rest/UserResource.java
+++ src/main/java/es/upm/miw/devops/rest/UserResource.java
@@ -4,11 +4,6 @@ import es.upm.miw.devops.data.model.User;
 import es.upm.miw.devops.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -17,6 +12,7 @@ import java.util.List;
 public class UserResource {
     public static final String USER = "/user";
     static final String USER_ID = "/{id}";
+    static final String USER_ID_ACTIVE = USER_ID + "/active";
 
     private final UserService userService;
 
@@ -30,9 +26,15 @@ public class UserResource {
                               @RequestParam(required = false) Boolean billable) {
         return this.userService.findAll(role, province, billable);
     }
+
     @GetMapping(USER_ID)
     public User read(@PathVariable String id) {
         return this.userService.findById(id);
+    }
+
+    @PutMapping(USER_ID_ACTIVE)
+    public User activate(@PathVariable String id) {
+        return this.userService.activate(id);
     }
 
     @DeleteMapping(USER_ID)
